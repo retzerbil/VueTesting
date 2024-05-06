4b3f7368c0bc6d943c5a84287ffc4123c261233a
Andreas Retzius <andreasretzius@gmail.com>
2024-05-06 11:09:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const buttonText = 'Spara';
+const clickable = true;
+const myClass = "testing";
+const showP = true;
+const numbers = [1,2,3,3,4,5];
+
+function yeah (){
+  console.log('yeah');
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    //v-bind and v-on
+    <button :disabled="!clickable" :class="myClass" @click="yeah">{{ buttonText }}</button>
+
+    //v-if
+    <p v-if="showP">This is a paragraph</p>
+
+    <span v-else>hello</span>
+
+    <ul>
+      //v-for
+      <li v-for="n in numbers">{{ n }}</li>
+    </ul>
+
+    </div>
+    
+</template>
+
+<style scoped>
+</style>

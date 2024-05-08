7f7edf1134b481a2a94b86bb8b7a04386f4aaa1e
Andreas Retzius <andreasretzius@gmail.com>
2024-05-08 10:14:53 +0200

    state management testing and onMounted testing.

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
 const buttonText = 'Spara';
 const clickable = true;
 const myClass = "testing";
-const showP = true;
-const numbers = [1,2,3,3,4,5];
+let showP = ref(true);
+const numbers = [1, 2, 3, 3, 4, 5];
+const names = ['John', 'Doe', 'Jane', 'Dee'];
 
-function yeah (){
+onMounted(() => {
+  console.log('mounted');
+});
+
+function yeah() {
   console.log('yeah');
+  showP.value = !showP.value;
 }
 </script>
 
 <template>
   <div class="wrapper">
-    //v-bind and v-on
+    <!-- v-bind and v-on -->
     <button :disabled="!clickable" :class="myClass" @click="yeah">{{ buttonText }}</button>
 
-    //v-if
+    <!-- v-if -->
     <p v-if="showP">This is a paragraph</p>
 
     <span v-else>hello</span>
 
     <ul>
-      //v-for
+      <!-- v-for -->
       <li v-for="n in numbers">{{ n }}</li>
     </ul>
 
+    <div v-for="n in names">
+      <h1>{{ n }}</h1>
     </div>
-    
+
+  </div>
+
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
